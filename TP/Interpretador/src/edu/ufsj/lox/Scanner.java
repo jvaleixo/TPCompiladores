13424b185d8e718c7a88b79a221ab693a8340bbd
Juan <juanvaleixo@gmail.com>
2023-03-28 16:35:25 -0300

    Adicionando Token, TokenTYpe e Scanner. Scanner ainda nao esta completo

---
+++ TP/Interpretador/src/edu/ufsj/lox/Scanner.java
@@ -0,0 +1,32 @@
+package edu.ufsj.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static edu.ufsj.lox.TokenType.*;
+
+class Scanner{
+
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    private final String source;
+    private final List<Token> tokens = new ArrayList<Token>();
+
+    Scanner(String source){
+        this.source = source;
+    }
+
+    List<Token> scanTokens(){
+        while(!isAtEnd()){
+          // inicio do proximo lexema
+          start = current;
+          scanToken();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tonkes;
+    }
+}
