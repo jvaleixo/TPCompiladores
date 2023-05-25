7a16d316f773417347e753a38d6f99d7816a031d
Juan <juanvaleixo@gmail.com>
2023-05-25 16:10:46 -0300

    Mapeamento de regras da gramatica

--- TP/Interpretador/src/edu/ufsj/lox/Parser.java
+++ TP/Interpretador/src/edu/ufsj/lox/Parser.java
@@ -25,7 +25,7 @@ class Parser {
       expr = new Expr.Binary(expr, operator, right);
     }
 
-    return Expr;
+    return expr;
   }
 
   private boolean match(TokenType... types){
@@ -59,5 +59,70 @@ class Parser {
 
   private Token previous(){
     return tokens.get(current - 1);
+  }
+
+  private Expr comparison(){
+    Expr expr = term();
+
+    while(match(GREATER, GREATER_EQUAL,LESS, LESS_EQUAL)){
+      Token operator = previous();
+      Expr right = term();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr term(){
+    Expr expr = factor();
+
+    while(match(MINUS, PLUS)){
+      Token operator = previous();
+      Expr right = factor();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr factor(){
+    Expr expr = unary();
+
+    while(match(SLASH, STAR)){
+      Token operator = previous();
+      Expr right = unary();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr unary(){
+    if(match(BANG,MINUS)){
+      Token operator = previous();
+      Expr right = unary();
+      return new Expr.Unary(operator,right);
+    }
+
+    return primary();
+  }
+
+  private Expr primary(){
+    if(match(FALSE)) return new Expr.Literal(false);
+    if(match(TRUE)) return new Expr.Literal(true);
+    if(match(NIL)) return new Expr.Literal(null);
+
+    if(match(NUMBER,STRING)){
+      return new Expr.Literal(previous().literal);
+    }
+
+    if(match(LEFT_PAREN)){
+      Expr expr = expression();
+      consume(RIGHT_PAREN, "Expect ')' after expression.");
+
+      return new Expr.Grouping(expr);
+    }
+
+    return null;
   }
 }
